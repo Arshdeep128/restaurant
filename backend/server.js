263e9dc8b65406f5e704825a682806f21abb626e
Arshdeep Kaur 200477055 <86702090+Arshdeep128@users.noreply.github.com>
2022-08-03 21:23:58 -0500

    changes

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require( "./config/db.js");
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => {
+
+    res.send("Api is running");
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
